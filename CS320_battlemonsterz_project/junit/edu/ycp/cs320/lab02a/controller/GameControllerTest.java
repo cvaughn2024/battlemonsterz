b942c700d40ec1048db73393fa5b5eb1606e4c4e
cvaughn2024 <124750926+cvaughn2024@users.noreply.github.com>
2023-03-18 13:00:58 -0400

    Add files via upload

---
+++ CS320_battlemonsterz_project/junit/edu/ycp/cs320/lab02a/controller/GameControllerTest.java
@@ -0,0 +1,26 @@
+package edu.ycp.cs320.lab02a.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.lab02a.controller.GameController;
+import edu.ycp.cs320.lab02a.model.Card;
+
+public class GameControllerTest {
+	private GameController controller;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	// test controller methods
+	public void testPlay() {
+		
+	}
+	
+	
+}
